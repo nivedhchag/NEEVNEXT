3f8ff6086db8e9f1721460a7206d227616246616
Nivedhan <chagananivedhan@gmail.com>
2025-02-17 08:02:00 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Hero from './Components/Hero';
 import Services from './Components/Services';
 import WhyChooseUs from './Components/WhyChooseUs';
 import Portfolio from './Components/Portfolio';
-import Testimonials from './Components/Testimonials';
 import FooterSection from './Components/FooterSection';
 import './index.css';
 
@@ -21,7 +20,7 @@ function App() {
   );
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <div className="min-h-screen bg-black">
         <div className="fixed inset-0 bg-gradient-to-br from-purple-600/20 via-transparent to-orange-500/20 pointer-events-none" />
         <Navbar />
